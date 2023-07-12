0d0ee0531a19beb28032f190f9a734d4c266ec89
unknown <sonalid6801@gmail.com>
2023-07-12 18:25:52 +0530

    Added all todo fies

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
 
+ import { Card } from '@mui/material';
+import './App.css';
+// import Button from './Button'; 
+// import Input from './Components/Input'
+// import Todo from './Todo'
+// import Lc from './Leaving_cer/Lc'
+import Temp from './Components/Temp'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<>
+   <Temp/>
+   </>
   );
 }
 
